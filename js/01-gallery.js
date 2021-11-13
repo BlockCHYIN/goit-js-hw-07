87a5c609b32d66b392afcf149cea2881cefeb435
BlockCHYIN <anatoliimarkovskii@gmail.com>
2021-11-13 22:03:42 +0200

    содержит JS

---
+++ js/01-gallery.js
@@ -0,0 +1,69 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+// console.log(createGalleryItemsMarkup(gallery));
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener('click', onOpenClick);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src=" ${preview} "
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+</div>
+    `;
+    }).join("");
+
+    console.log(createGalleryItemsMarkup);
+}
+
+
+function onOpenClick (event) {
+  
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+const url = event.target.dataset.source;
+
+ const instance = basicLightbox.create(
+`
+<img src='${url}' width="800" height="600"> 
+`,
+{
+	onShow: (instance) => {
+window.addEventListener('keydown', (event) => {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    instance.close();
+  }
+});
+   },
+  
+ })
+ instance.show();
+ }
+
+
+
+
+
+
+
+
+
